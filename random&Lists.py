4b5357c2898c9d9852409ce043efd8d54138328a
rameshchauhan01 <46587745+rameshchauhan01@users.noreply.github.com>
2020-03-02 21:14:48 +0530

    Partition arrays and methods

--- random&Lists.py
+++ random&Lists.py
@@ -63,3 +63,13 @@ print(Sorted_list(l2))
 l1,l2=l2,l1
 print (l1)
 print (l2)
+
+#append all zeros at the end  of array
+def moveZeros(arr):
+    return [nonZero for nonZero in arr if nonZero != 0] + [Zero for Zero in arr if Zero == 0]
+
+# Driver function
+if __name__ == "__main__":
+    arr = [1, 2, 0, 4, 3, 0, 5, 0]
+    print(moveZeros(arr))
+
