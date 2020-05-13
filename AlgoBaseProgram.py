dbc104a8ec4331b150ef1223335882ffc86e24c1
rameshchauhan01 <46587745+rameshchauhan01@users.noreply.github.com>
2020-05-13 15:47:01 +0530

    Update AlgoBaseProgram.py

--- AlgoBaseProgram.py
+++ AlgoBaseProgram.py
@@ -54,3 +54,62 @@ def bubbleSort(arr):
 arr = [64, 34, 25, 12, 22, 11, 90]
 print(bubbleSort(arr))
 
+# Binary Search function and unit test class to test the function
+# Print the search value index in the sequence
+import unittest
+
+def binary_search(array, t):
+    l = 0
+    h = len(array) - 1
+
+    while (l <= h):
+        m = (l + h) // 2
+        if (array[m] == t):
+            return m
+        elif (array[m] < t):
+            l = m + 1
+        else:
+            h = m - 1
+
+    return -1
+
+print(binary_search([5,3,1,2,4],4))
+print(binary_search([5,3,1,2,4],6))
+
+class TestBinarySearch(unittest.TestCase):
+
+    def test_binary_search(self):
+        self.assertEqual(binary_search([], 3), -1)
+        self.assertEqual(binary_search([1, 2], 3), -1)
+        self.assertEqual(binary_search([1, 2, 3, 4], 3), 2)
+        self.assertEqual(binary_search([1, 2, 3, 4, 5], 3), 2)
+        self.assertEqual(binary_search([1, 2, 3, 4], 3), 2)
+        self.assertEqual(binary_search([1, 2, 3, 4], 1), 0)
+        self.assertEqual(binary_search([1, 2, 3, 4], 4), 3)
+
+# Linear search function and unit test class for testing the linear search function.
+def linear_search(arr,item):
+
+    for i in range(len(arr)):
+        if arr[i]==item:
+            return True
+
+    return False
+
+# Linear search Driver code
+'''printing the linear_search function retuns values on sample test data'''
+arrlist=[3,2,78,34,23]
+item=78
+print(linear_search(arrlist,item))
+'''unit test cases in one function for the linear_search function'''
+# Linear search unit test
+class TestLinearSearch(unittest.TestCase):
+
+    def test_linear_search(self):
+        self.assertEqual(linear_search([], 3), False)
+        self.assertEqual(linear_search([1, 2], 3), False)
+        self.assertEqual(linear_search([1, 2, 3, 4], 3), True)
+        self.assertEqual(linear_search([1, 2, 3, 4, 5], 5), True)
+
+if __name__ == '__main__':
+    unittest.main()
